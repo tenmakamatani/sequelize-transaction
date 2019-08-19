8c4c46c3b550c8ff7603372165a27ce28af7ca1e
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-19 11:47:10 +0900

    [update]トランザクション処理を実装

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const sequelize = require("./models/sequelize-loader").database;
 const User = require("./models/user");
 const Product = require("./models/product");
 
@@ -10,4 +11,35 @@ User.sync().then(() => {
     });
 }).catch((err) => {
     console.log(err);
+});
+
+// commit, rollback は自動
+// Product.destroy と User.destroy の順番を逆にすると rollback の再現もできる
+sequelize.transaction(async (tx) => {
+
+    await User.upsert({
+        userId: 1,
+        userName: "user"
+    }, { transaction: tx });
+
+    await Product.upsert({
+        productId: 1,
+        productName: "product",
+        boughtBy: 1
+    }, { transaction: tx });
+
+    await Product.destroy({
+        where: {
+            productId: 1
+        },
+        transaction: tx
+    });
+
+    await User.destroy({
+        where: {
+            userId: 1
+        },
+        transaction: tx
+    });
+
 });
--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const Sequelize = require("sequelize");
+const databaseInfo = require("../database.json");
 
-const sequelize = new Sequelize("sequelize_transaction", "root", "root", { dialect: "mysql" });
+const sequelize = new Sequelize("sequelize_transaction", databaseInfo.name, databaseInfo.password, { dialect: "mysql" });
 
 module.exports = {
     database: sequelize,
